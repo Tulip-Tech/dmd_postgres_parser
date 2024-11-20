cc2dcccf3123ddd868f0ea71fc5f11e8532c6b5d
mmarifat <mma.rifat66@gmail.com>
2024-11-20 15:29:31 +0000

    chore: dm+d postgres parser

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 >
 >Aneurin Bevan, _In Place of Fear_
 
-NHS Dictionary of medicines and devices (dm+d) SQL parser
+NHS Dictionary of medicines and devices (dm+d) Postgres SQL parser
 
-These Python and SQL scripts are used to parse the dm+d releases in XML format into a MySQL database. Relational fields are referenced together using foreign keys, and the description of the columns are stored in their comments.
+These Python and Postgres SQL scripts are used to parse the dm+d releases in XML format into a MySQL database. Relational fields are referenced together using foreign keys, and the description of the columns are stored in their comments.
 
 You can download the official releases of dm+d for free from https://isd.digital.nhs.uk.
 
 ## Usage
-Install MySQL server, unzip the dm+d releases into a folder and clone this repo.
-
-Run `python parser.py -u [your mysql username] -p [your mysql password] -d [relative or absolute path to the directory containing the dm+d folders]`
+1. Install PostgresSQL server, unzip the dm+d releases into a folder and clone this repo.
+2. Run `pip install -r requirements.txt` to install the required Python packages.
+3. Run `python parser.py -u [your postgres username] -p [your postgres password] -d [relative or absolute path to the directory containing the dm+d folders]`
--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import glob
 import os
 import xml.etree.ElementTree as ET
 
-import mysql.connector
+import psycopg2
 
 
 def monotabular(path, concept_name):
@@ -11,16 +11,16 @@ def monotabular(path, concept_name):
     root = tree.getroot()
     section = root
 
-    table_name = section.tag
-    print("Populating table " + concept_name + "$" + table_name.lower())
+    table_name = str(section.tag).lower()
+    print("Populating table " + concept_name + "_" + table_name)
     for entry in section:
         columns = []
         values = []
         for field in entry:
             columns.append(field.tag)
             values.append(field.text)
-        clmn = ', '.join(["`{}`".format(value) for value in columns])
-        sql = "INSERT INTO `{}` ({}) VALUES ({})".format(concept_name + "$" + table_name, clmn,
+        clmn = ', '.join(['"{}"'.format(value) for value in columns])
+        sql = 'INSERT INTO "{}" ({}) VALUES ({})'.format(concept_name + "_" + table_name, clmn,
                                                          ','.join(['%s'] * len(columns)))
         cursor.execute(sql, values)
 
@@ -29,7 +29,7 @@ def polytabular(path, concept_name):
     tree = ET.parse(path)
     root = tree.getroot()
     for section in root:
-        table_name = section.tag
+        table_name = str(section.tag).lower()
         print("Populating table " + table_name)
         for entry in section:
             columns = []
@@ -37,8 +37,8 @@ def polytabular(path, concept_name):
             for field in entry:
                 columns.append(field.tag)
                 values.append(field.text)
-            clmn = ', '.join(["`{}`".format(value) for value in columns])
-            sql = "INSERT INTO `{}` ({}) VALUES ({})".format(concept_name + "$" + table_name, clmn,
+            clmn = ', '.join(['"{}"'.format(value) for value in columns])
+            sql = 'INSERT INTO "{}" ({}) VALUES ({})'.format(concept_name + "_" + table_name, clmn,
                                                              ','.join(['%s'] * len(columns)))
             cursor.execute(sql, values)
 
@@ -47,27 +47,36 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--directory', type=str, required=True,
                     help='directory containing the unzipped nhsbsa_dmd... folders')
 parser.add_argument('-u', '--username', type=str, required=True,
-                    help='username of your mysql database')
+                    help='username of your PostgreSQL database')
 parser.add_argument('-p', '--password', type=str, required=True,
-                    help='password of the mysql database corresponding to the username')
+                    help='password of the PostgreSQL database corresponding to the username')
+parser.add_argument('-H', '--host', type=str, default='localhost',
+                    help='host of the PostgreSQL database')
+parser.add_argument('-P', '--port', type=int, default=5432,
+                    help='port of the PostgreSQL database')
 args = parser.parse_args()
 
 rootpath = args.directory
 username = args.username
 password = args.password
+host = args.host
+port = args.port
 
-cnx = mysql.connector.connect(user=username, password=password, database='sys')
+cnx = psycopg2.connect(user=username, password=password, host=host, port=port, dbname='postgres')
+cnx.autocommit = True
 cursor = cnx.cursor()
 
-table_structure_path = os.path.dirname(os.path.realpath(__file__)) + '\\dmd_structure.sql'
+table_structure_path = os.path.dirname(os.path.realpath(__file__)) + '/dmd_structure.sql'
 
-concepts = [("f_lookup2_3{}.xml", "lookup", polytabular),
-            ("f_ingredient2_3{}.xml", "ingredient", monotabular),
-            ("f_vtm2_3{}.xml", "vtm", monotabular),
-            ("f_vmp2_3{}.xml", "vmp", polytabular),
-            ("f_amp2_3{}.xml", "amp", polytabular),
-            ("f_vmpp2_3{}.xml", "vmpp", polytabular),
-            ("f_ampp2_3{}.xml", "ampp", polytabular)]
+concepts = [
+    ("f_lookup2_3{}.xml", "lookup", polytabular),
+    ("f_ingredient2_3{}.xml", "ingredient", monotabular),
+    ("f_vtm2_3{}.xml", "vtm", monotabular),
+    ("f_vmp2_3{}.xml", "vmp", polytabular),
+    ("f_amp2_3{}.xml", "amp", polytabular),
+    ("f_vmpp2_3{}.xml", "vmpp", polytabular),
+    ("f_ampp2_3{}.xml", "ampp", polytabular)
+]
 
 os.chdir(rootpath)
 dmddirs = glob.glob("nhsbsa_dmd_*_*")
@@ -78,16 +87,14 @@ for dmddir in dmddirs:
         print("Skipping " + dmddir)
         continue
 
-    schema_name = dmddir[7:-6].replace('.', '-')  # no '.' in schema name so we don't have to use back ticks
+    schema_name = dmddir[7:-6].replace('.', '_')  # no '.' in schema name so we don't have to use quotes
     print("Creating schema " + schema_name)
-    cursor.execute(
-        'CREATE DATABASE  IF NOT EXISTS `{}` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;'.format(
-            schema_name))
-    cursor.execute('USE `{}`;'.format(schema_name))
+    cursor.execute('CREATE SCHEMA IF NOT EXISTS "{}";'.format(schema_name))
+    cursor.execute('SET search_path TO "{}";'.format(schema_name))
 
     print("Importing table structure")
-    for line in open(table_structure_path):
-        cursor.execute(line)
+    with open(table_structure_path, 'r') as f:
+        cursor.execute(f.read())
 
     print("Parsing xml...")
     os.chdir(dmddir)
@@ -95,7 +102,6 @@ for dmddir in dmddirs:
         filename = glob.glob(con[0].format("*"))[0]
         print("Parsing concept " + con[1])
         con[2](filename, con[1])
-    os.chdir(rootpath)
 
     cnx.commit()
     print("Done")
